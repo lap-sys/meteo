f6fab359aaffcd69ad80bc3016e3678fa197a6f8
Louis-amaury Pelissier <louis-amaurypelissier@Louis-amaurys-MacBook-Pro.local>
2022-08-24 00:46:30 +0800

    added streamlit app

--- main.py
+++ main.py
@@ -1,126 +1,130 @@
 
-import pandas as pd
 import sys
 import streamlit as st
-import pandas as pd
-import numpy as np
-import time
-import math
-from sklearn.model_selection import train_test_split
-from neuralprophet import NeuralProphet
-from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.svm import SVC
-import statsmodels.api as sm
-from sklearn.preprocessing import StandardScaler, normalize
-from imblearn.over_sampling import RandomOverSampler, SMOTE
+# import pandas as pd
+# import numpy as np
+# import time
+# import math
+import pickle
+# from sklearn.model_selection import train_test_split
+# from neuralprophet import NeuralProphet
+# from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+# from sklearn.svm import SVC
+# import statsmodels.api as sm
+# from sklearn.preprocessing import StandardScaler, normalize
+# from imblearn.over_sampling import RandomOverSampler, SMOTE
 st.write("Météo Forecast")
 region =  st.selectbox(
     'Select an Australian city',
  ('Albury', 'BadgerysCreek', 'Cobar', 'CoffsHarbour', 'Moree', 'Newcastle', 'NorahHead', 'NorfolkIsland', 'Penrith', 'Richmond', 'Sydney', 'SydneyAirport', 'WaggaWagga', 'Williamtown', 'Wollongong', 'Canberra', 'Tuggeranong', 'MountGinini', 'Ballarat', 'Bendigo', 'Sale', 'MelbourneAirport', 'Melbourne', 'Mildura', 'Nhil', 'Portland', 'Watsonia', 'Dartmoor', 'Brisbane', 'Cairns', 'GoldCoast', 'Townsville', 'Adelaide', 'MountGambier', 'Nuriootpa', 'Woomera', 'Albany','Witchcliffe', 'PerthAirport', 'Perth', 'SalmonGums', 'Walpole','Hobart', 'Launceston','AliceSprings','Darwin','Katherine', 'Uluru'))
 
-df = pd.read_csv('./weatherAUS.csv')
-df['Date'] = pd.to_datetime(df['Date'])
-df['year'] = df['Date'].dt.year
-df['month'] = df['Date'].dt.month
-df['day'] = df['Date'].dt.day
-df.drop('Date', axis = 1, inplace = True)
+# dff = pd.read_csv('./weatherAUS.csv')
+# df = dff
+# df['Date'] = pd.to_datetime(df['Date'])
+# df['year'] = df['Date'].dt.year
+# df['month'] = df['Date'].dt.month
+# df['day'] = df['Date'].dt.day
+# df.drop('Date', axis = 1, inplace = True)
 
 
-categorical_features = [column_name for column_name in df.columns if df[column_name].dtype == 'O']
+# categorical_features = [column_name for column_name in df.columns if df[column_name].dtype == 'O']
 
-numerical_features = [column_name for column_name in df.columns if df[column_name].dtype != 'O']
+# numerical_features = [column_name for column_name in df.columns if df[column_name].dtype != 'O']
 
-categorical_features_with_null = [feature for feature in categorical_features if df[feature].isnull().sum()]
+# categorical_features_with_null = [feature for feature in categorical_features if df[feature].isnull().sum()]
 
-for each_feature in categorical_features_with_null:
-    mode_val = df[each_feature].mode()[0]
-    df[each_feature].fillna(mode_val,inplace=True)
+# for each_feature in categorical_features_with_null:
+#     mode_val = df[each_feature].mode()[0]
+#     df[each_feature].fillna(mode_val,inplace=True)
 
-print(df[numerical_features].describe())
+# print(df[numerical_features].describe())
 
-features_with_outliers = ['MaxTemp', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Pressure3pm']
+# features_with_outliers = ['MaxTemp', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Pressure3pm']
 
-for feature in features_with_outliers:
-    q1 = df[feature].quantile(0.25)
-    q3 = df[feature].quantile(0.75)
-    IQR = q3-q1
-    lower_limit = q1 - (IQR*1.5)
-    upper_limit = q3 + (IQR*1.5)
-    df.loc[df[feature]<lower_limit,feature] = lower_limit
-    df.loc[df[feature]>upper_limit,feature] = upper_limit
-
-
-numerical_features_with_null = [feature for feature in numerical_features if df[feature].isnull().sum()]
-
-for feature in numerical_features_with_null:
-    mean_value = df[feature].mean()
-    df[feature].fillna(mean_value,inplace=True)
-
-df['RainToday'].replace({'No':0, 'Yes': 1}, inplace = True)
+# for feature in features_with_outliers:
+#     q1 = df[feature].quantile(0.25)
+#     q3 = df[feature].quantile(0.75)
+#     IQR = q3-q1
+#     lower_limit = q1 - (IQR*1.5)
+#     upper_limit = q3 + (IQR*1.5)
+#     df.loc[df[feature]<lower_limit,feature] = lower_limit
+#     df.loc[df[feature]>upper_limit,feature] = upper_limit
 
 
-df['RainTomorrow'].replace({'No':0, 'Yes': 1}, inplace = True)
-df['RainTomorrow'].value_counts().plot(kind='bar')
-def encode_data(feature_name):
-    mapping_dict = {}
-    unique_values = list(df[feature_name].unique())
-    for idx in range(len(unique_values)):
-        mapping_dict[unique_values[idx]] = idx
-    print(mapping_dict)
-    return mapping_dict
+# numerical_features_with_null = [feature for feature in numerical_features if df[feature].isnull().sum()]
 
-df['WindGustDir'].replace(encode_data('WindGustDir'),inplace = True)
-df['WindDir9am'].replace(encode_data('WindDir9am'),inplace = True)
-df['WindDir3pm'].replace(encode_data('WindDir3pm'),inplace = True)
-df['Location'].replace(encode_data('Location'), inplace = True)
+# for feature in numerical_features_with_null:
+#     mean_value = df[feature].mean()
+#     df[feature].fillna(mean_value,inplace=True)
+
+# df['RainToday'].replace({'No':0, 'Yes': 1}, inplace = True)
 
 
-target = 'RainTomorrow'
-df = df[['Location','month', 'MaxTemp','RainTomorrow', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Pressure3pm']]
-y = df[target]
-X = df.drop([target],axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# df['RainTomorrow'].replace({'No':0, 'Yes': 1}, inplace = True)
+# df['RainTomorrow'].value_counts().plot(kind='bar')
+# def encode_data(feature_name):
+#     mapping_dict = {}
+#     unique_values = list(df[feature_name].unique())
+#     for idx in range(len(unique_values)):
+#         mapping_dict[unique_values[idx]] = idx
+#     print(mapping_dict)
+#     return mapping_dict
+
+# df['WindGustDir'].replace(encode_data('WindGustDir'),inplace = True)
+# df['WindDir9am'].replace(encode_data('WindDir9am'),inplace = True)
+# df['WindDir3pm'].replace(encode_data('WindDir3pm'),inplace = True)
+# df['Location'].replace(encode_data('Location'), inplace = True)
 
 
-smo = SMOTE()
-X_sm, y_sm = smo.fit_resample(X_train, y_train)
-print('Classes échantillon SMOTE :', dict(pd.Series(y_sm).value_counts()))
-
-classifier = GradientBoostingClassifier()
-classifier.fit(X_sm, y_sm)
-
-df = pd.read_csv('./weatherAUS.csv')
-
-
-df.Location.unique()
-df['Date'] = pd.to_datetime(df['Date'])
-df['Year'] = df['Date'].apply(lambda x: x.year)
-data = df
+# target = 'RainTomorrow'
+# df = df[['Location','month', 'MaxTemp','RainTomorrow', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Pressure3pm']]
+# y = df[target]
+# X = df.drop([target],axis=1)
+# X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
 
 
-df['year'] = df['Date'].dt.year
-df['month'] = df['Date'].dt.month
-df['day'] = df['Date'].dt.day
+# # smo = SMOTE()
+# # X_sm, y_sm = smo.fit_resample(X_train, y_train)
+# # print('Classes échantillon SMOTE :', dict(pd.Series(y_sm).value_counts()))
+
+# classifier = GradientBoostingClassifier()
+# # classifier.fit(X_sm, y_sm)
+# classifier.fit(X_test, y_test)
+# with open('classifier.pickle', 'wb') as f:
+#     pickle.dump(classifier, f)
+# df = pd.read_csv('./weatherAUS.csv')
 
 
-d = data[data['Location'] == region]
-d = d[['Date', 'Rainfall']]
-d.dropna(inplace=True)
-
-d.columns = ['ds', 'y'] 
-
-mod = NeuralProphet()
-model = mod.fit(d, freq='D')
+# df.Location.unique()
+# df['Date'] = pd.to_datetime(df['Date'])
+# df['Year'] = df['Date'].apply(lambda x: x.year)
+# data = df
 
 
+# df['year'] = df['Date'].dt.year
+# df['month'] = df['Date'].dt.month
+# df['day'] = df['Date'].dt.day
+
+
+# d = data[data['Location'] == region]
+# d = d[['Date', 'Rainfall']]
+# d.dropna(inplace=True)
+
+# d.columns = ['ds', 'y'] 
+
+# mod = NeuralProphet()
+# model = mod.fit(d, freq='D')
+# with open('ts.pickle', 'wb') as f:
+#     pickle.dump(mod, f)
 
 
 
+prd = 0
 option =  st.selectbox(
     'Select how many days/weeks you want the forecast on',
  ('Tomorrow', 'Next week', 'Next Month'))
@@ -145,7 +149,7 @@ if prd == 1:
   df_test['WindGustSpeed'] = st.slider('Wind', 5, 150, 10)
   df_test['Rainfall'] = st.slider('Rainfall', 0, 500,20)
   df_test['month'] = 8
-
+  df_test
 else:
   target = st.selectbox('predict',
     ('MaxTemp', 'Rainfall'))
@@ -153,14 +157,18 @@ else:
 
 if st.button('Forecast'):
   if prd == 1:
+    with open('classifier.pickle') as f:
+      classifier = pickle.load(f)
     y_pred = classifier.predict(X_test)
-    print(y_pred)
+    y_pred[0]
     # st.write('Raining tomorrow' if y_pred['RainTomorrow'] == 1 else 'Not raining tomorrow')
   else:
+    with open('ts.pickle') as f:
+      mod = pickle.load(f)
     future = mod.make_future_dataframe(d, periods=prd, n_historic_predictions=True)
     prediction = mod.predict(future)
     forecast = mod.plot(prediction)
-    st.write('Predicted ' + target + ' in ' + prd + 'days is:' + prediction[-1])
+    st.write('Predicted ' + target + ' in ' + str(prd) + 'days is:' + str(prediction))
 
 
 
